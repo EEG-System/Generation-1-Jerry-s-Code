0ceb46c048e9b5ac337903c3bce091a974bb68e5
Jose Carlos Dominguez <jcdc1carlos@gmail.com>
2020-11-18 14:43:00 -0700

    Add files via upload

--- EEG_MSP_EXP432E401Y_nortos_ccs2/usb_dev_bulk.c
+++ EEG_MSP_EXP432E401Y_nortos_ccs2/usb_dev_bulk.c
@@ -1,34 +1,4 @@
-/* --COPYRIGHT--,BSD
- * Copyright (c) 2017, Texas Instruments Incorporated
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * --/COPYRIGHT--*/
+
 //*****************************************************************************
 //
 // usb_dev_bulk.c - Main routines for the generic bulk device example.
@@ -207,7 +177,7 @@ static uint32_t EchoNewDataToHost(tUSBDBulkDevice *psDevice, uint8_t *pi8Data,
         g_pui8USBTxBuffer[ui32WriteIndex + 1] = (char) Registers[g_pui8USBRxBuffer[ui32ReadIndex + 1]]; //Send back the Result
         g_pui8USBTxBuffer[ui32WriteIndex + 2] = (char) Registers[g_pui8USBRxBuffer[ui32ReadIndex + 1]]; //Send back the Result
         break;
-    case 'W': //Write Register
+    case 'W': //Write Registerms
         if (g_pui8USBRxBuffer[ui32ReadIndex + 1] == '{') // If a bracket is received then write the same value to all the registers
         {
             WriteREGS(0x05, 0x0C, g_pui8USBRxBuffer[ui32ReadIndex + 1]);
